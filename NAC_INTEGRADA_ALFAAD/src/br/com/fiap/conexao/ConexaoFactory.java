5f548f403afaae1526f8500f0e7e3d355d5cb4ba
Felipe Vieira <fel.vieira@gmail.com>
2016-10-04 16:18:42 -0300

    Update ConexaoFactory.java

--- NAC_INTEGRADA_ALFAAD/src/br/com/fiap/conexao/ConexaoFactory.java
+++ NAC_INTEGRADA_ALFAAD/src/br/com/fiap/conexao/ConexaoFactory.java
@@ -6,9 +6,8 @@ import java.sql.DriverManager;
 public class ConexaoFactory {
 
 	public Connection getConnection () throws Exception{
-		//return DriverManager.getConnection("jdbc:oracle:thin:/:@192.168.60.15:1521:ORCL");
-		//return DriverManager.getConnection("jdbc:oracle:thin:/:@localhost:1521:XE","Felps","fe1416");
-		return DriverManager.getConnection("jdbc:oracle:thin:/:@oracle.fiap.com.br:1521:ORCL","OPS$RM76193","140487");
+		return DriverManager.getConnection("jdbc:oracle:thin:/:@192.168.60.15:1521:ORCL");
+		
 	}
 
 
