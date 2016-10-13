192ee69d11964852cdf90dca96520c6ffbc304f5
Felipe Moita Vieira <fel.vieira@gmail.com>
2016-10-13 10:56:56 -0300

    Teste ForumDAO e gravar

--- NAC_INTEGRADA_ALFAAD/src/br/com/fiap/dao/ForumDAO.java
+++ NAC_INTEGRADA_ALFAAD/src/br/com/fiap/dao/ForumDAO.java
@@ -1,5 +1,35 @@
 package br.com.fiap.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import br.com.fiap.beans.Forum;
+
 public class ForumDAO extends PessoaDAO{
 
+	public void gravar (Forum f, Connection conexao ) throws Exception{
+		 
+    	PreparedStatement strt = conexao.prepareStatement("insert into T_AAD_PESSOA "
+				+ "(CD_PESSOA, NM_NOME) "
+				+ "values (SQ_AM_PESSOA.nextval,?)");
+    	
+    	strt.setString(1, f.getNome());
+    	
+    	strt.execute();
+    	
+        PreparedStatement estrutura = conexao.prepareStatement("insert into T_AAD_FORUM "
+        		+ "(DS_FORUM,CD_FORUM) "
+        		+ "values (?,SQ_AM_PESSOA.currval)");
+ 
+        
+        	
+            estrutura.setString(1, f.getDescricao());
+            
+            estrutura.execute();
+            estrutura.close();
+ 
+       System.out.println("Gravado");
+ 
+    }
+	
 }
