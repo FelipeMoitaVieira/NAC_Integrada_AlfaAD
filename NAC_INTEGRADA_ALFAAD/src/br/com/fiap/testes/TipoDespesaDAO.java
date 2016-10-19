d0affdd550f5622a5cdfca907612336ccd400a7a
João Miguel <joao.martins789@gmail.com>
2016-10-19 10:11:19 -0200

    Tipo de Despesa DAO

---
+++ NAC_INTEGRADA_ALFAAD/src/br/com/fiap/testes/TipoDespesaDAO.java
@@ -0,0 +1,39 @@
+package br.com.fiap.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.fiap.beans.TipoDespesa;
+
+public class TipoDespesaDAO {
+
+	public List <TipoDespesa> getLista (Connection conexao) throws Exception{
+		List<TipoDespesa> lstTipoDespesa = new ArrayList<TipoDespesa>();
+		PreparedStatement stmt = conexao.prepareStatement("SELECT TD.CD_TIPO_DESPESA, TD.DS_TIPO_DESPESA FROM T_AAD_TIPO_DESPESA TD");
+		ResultSet resultadoDados = stmt.executeQuery();
+		
+		
+		while (resultadoDados.next()) {
+			TipoDespesa tipoDespesa = new TipoDespesa();
+			
+			tipoDespesa.setCodigoTipoDespesa(resultadoDados.getInt("CD_TIPO_DESPESA"));
+			tipoDespesa.setDescricaoTipoDespesa(resultadoDados.getString("DS_TIPO_DESPESA"));
+			lstTipoDespesa.add(tipoDespesa);
+				
+		}
+		
+		
+		resultadoDados.close();
+		stmt.close();
+		return lstTipoDespesa;
+		
+		
+	}
+
+	
+	
+	
+}
